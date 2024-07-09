511fca2ad82d25129ea6373730b63f6936a1eda2
Wladimir Schkulov <wladimir.schkulov1995@gmail.com>
2024-07-09 11:08:14 +0200

    solved first challenge

---
+++ js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,15 @@
+console.clear();
+
+const url = "https://swapi.py4e.com/api/people";
+
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.results[0].name);
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+fetchData();
