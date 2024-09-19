25e1580ddf46510816018769369abef5426f7fa8
Wladimir Schkulov <wladimir.schkulov1995@gmail.com>
2024-09-19 15:40:43 +0200

    backendEditDelete

---
+++ backend-update-and-delete_products/components/ProductForm/index.js
@@ -0,0 +1,73 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+import useSWR from "swr";
+
+export default function ProductForm(defaultValue) {
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    event.target.reset();
+  }
+
+  return (
+    <StyledForm onSubmit={handleAddProduct}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={defaultValue.name || ""}
+        />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input
+          type="text"
+          id="description"
+          name="description"
+          defaultValue={defaultValue.description || ""}
+        />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input
+          type="number"
+          id="price"
+          name="price"
+          min="0"
+          defaultValue={defaultValue.price || 0}
+        />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
